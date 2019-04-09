bd1a2dace9114acf61766f251a8a280fe0083008
ATM006 <18829897162@163.com>
2019-04-09 08:38:30 +0800

    dev

--- src/entity/device.h
+++ src/entity/device.h
@@ -4,36 +4,35 @@
 
 class device
 {
-	private:
-	    device(int id, string name, string token);
-		device(int id, string name, string token, string site_name, string site_token);
-		~device();
+    private:
+        device(int id, string name, string token);
+        device(int id, string name, string token, string site_name, string site_token);
+        ~device();
+    
+    public:
+        bool set_id(int id);
+	    bool set_name(string name);
+        bool set_token(string token);
+	    bool set_site_name(string site_name);
+        bool set_site_token(string site_token);
 
-	public:
-		bool set_id(int id);
-		bool set_name(string name);
-		bool set_token(string token);
-		bool set_site_name(string site_name);
-		bool set_site_token(string site_token);
+	    int get_id();
+	    string get_name();
+	    string get_token();
+	    string get_site_name();
+	    string get_site_token();
 
-		int get_id();
-		string get_name();
-		string get_token();
-		string get_site_name();
-		string get_site_token();
-
-	private:
-		int _id;
-		string _name;
-		string _token;
-		//Location _location;
-		string _site_name;
-		string _site_token;
-		//DevData _dev_data;
-		
-
-	public:
-		#todo
+    private:
+        int _id;
+        string _name;
+        string _token;
+        //Location _location;
+        string _site_name;
+        string _site_token;
+        //DevData _dev_data;
+        
+    public:
+        #todo
 }
 
 
